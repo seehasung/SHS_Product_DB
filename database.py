6461627d72429a335f0f3f362ae497e6e84537d1
서하성 <shsboss274@gmail.com>
2025-10-22 22:50:47 +0900

    리뉴얼

--- database.py
+++ database.py
@@ -86,8 +86,7 @@ class Reference(Base):
 class Comment(Base):
     __tablename__ = "comments"
     id = Column(Integer, primary_key=True, index=True)
-    account_sequence = Column(Integer, nullable=False) # 계정 순번 필드 추가 (0=작성자, 1-8=계정)
-    text = Column(Text, nullable=False)
+    account_sequence = Column(Integer, nullable=False, server_default='0') # 기본값을 '0'으로 설정    text = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     
     reference_id = Column(Integer, ForeignKey("references.id", ondelete="CASCADE"))
