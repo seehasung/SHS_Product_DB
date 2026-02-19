1ffab95bbf6d560ce68565dd1f152382132b67a6
서하성 <shsboss274@gmail.com>
2026-02-19 15:26:02 +0900

    리뉴얼11

--- worker_agent.py
+++ worker_agent.py
@@ -51,7 +51,7 @@ from pathlib import Path
 class NaverCafeWorker:
     """네이버 카페 자동 작성 Worker"""
     
-    VERSION = "1.0.2" # 현재 버전
+    VERSION = "1.0.3" # 현재 버전
     
     def __init__(self, pc_number: int, server_url: str = "scorp274.com"):
         self.pc_number = pc_number
