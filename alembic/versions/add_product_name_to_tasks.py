5e1753d90b7c30ceea809f762c34e93ca0d19ad4
서하성 <shsboss274@gmail.com>
2026-02-28 16:36:48 +0900

    리뉴얼11

--- alembic/versions/add_product_name_to_tasks.py
+++ alembic/versions/add_product_name_to_tasks.py
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = 'add_product_name_to_tasks'
-down_revision = 'add_keyword_to_tasks'
+down_revision = 'add_schedule_logs'
 branch_labels = None
 depends_on = None
 
